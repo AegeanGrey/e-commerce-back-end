eaa2bc4127f87779ac417122b4d42e4bf1547dc2
Aegean <todddharni@gmail.com>
2023-07-24 22:37:39 -0600

    Added column data to 'ProductTag.js'

--- models/Product.js
+++ models/Product.js
@@ -38,7 +38,8 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category'
+        model: 'category',
+        key: 'id'
       }
     }
   },
